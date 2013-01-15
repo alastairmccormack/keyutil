88475291045a0255f505f38bc6b64d7146c50221
alastair.mccormack@gmail.com <alastair.mccormack@gmail.com@ab0a43bb-37de-2934-8d40-c1c96e2bb7c8>
2013-01-15 17:59:56 +0000

    FEATURE: Existing JKS files can be imported/merged FEATURE: Multiple muti-part PEM and JKS files can be merged FEATURE: List function

--- src/uk/co/mccnet/keyutil/JKSKeyStoreUtil.java
+++ src/uk/co/mccnet/keyutil/JKSKeyStoreUtil.java
@@ -12,39 +12,34 @@ import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.logging.Logger;
-
 import javax.security.auth.x500.X500Principal;
-
 import org.apache.commons.codec.binary.Base64;
 
 
 /**
- * Simple utility to import add PEM strings and PEMFile objects to Java JKS keystores and truststores
+ * Simple utility class to import PEM strings, PEM Strings and JKS files into a single JKS Keystore/Truststore
  * 
  * @see PEMFile
  *
  */
+
 public class JKSKeyStoreUtil {
 	File file;
 	KeyStore keyStore;
-	private static Logger logger = Logger.getLogger(JKSKeyStoreUtil.class.getCanonicalName());
-
-	char[] password;
-	boolean deferedSave = false;
 	
+	private static Logger logger = Logger.getLogger(Main.class.getName());
 	
 	/**
-	 * @param file 							new or existing JKS file
-	 * @param password						password to open JKS file
-	 * @param create						create JKS file if it does not exist 
 	 * @throws NoSuchAlgorithmException
 	 * @throws CertificateException
 	 * @throws IOException
 	 * @throws JKSKeyStoreUtilException
-	 */
-	protected JKSKeyStoreUtil(File file, String password, Boolean create) throws NoSuchAlgorithmException, CertificateException, IOException, JKSKeyStoreUtilException {
-		this.file = file;
+	 */	
+	public JKSKeyStoreUtil() throws NoSuchAlgorithmException, CertificateException, IOException, JKSKeyStoreUtilException {
 		
 		try {
 			keyStore = KeyStore.getInstance("JKS");
@@ -52,44 +47,48 @@ public class JKSKeyStoreUtil {
 			throw new JKSKeyStoreUtilException(e);
 		}
 		
-		if (password != null) this.password = password.toCharArray();
-		
-		if (create) {
-			keyStore.load(null);
-		} else {
-			FileInputStream fis = new FileInputStream(file);
-			keyStore.load(fis, this.password);
-			fis.close();
-		}
+		keyStore.load(null);
 	}
 	
-	/**
-	 * @param file							new JKS formatted key store
-	 * @param password						keystore password
-	 * @return								new <code>JKSKeyStoreUtil</code> object
-	 * @throws JKSKeyStoreUtilException
-	 */
-	static public JKSKeyStoreUtil newKeyStore(File file, String password) throws JKSKeyStoreUtilException {
-		try {
-			return new JKSKeyStoreUtil(file, password, true);
-		} catch (Exception e) {
-			throw new JKSKeyStoreUtilException(e);
-		}
-	}
 	
 	/**
-	 * Open existing JKS format file 
-	 * 
-	 * @param file							existing JKS formatted key store
-	 * @param password						keystore password
-	 * @return								<code>JKSKeyStoreUtil</code> object
 	 * @throws NoSuchAlgorithmException
 	 * @throws CertificateException
 	 * @throws IOException
 	 * @throws JKSKeyStoreUtilException
+	 */	
+	public JKSKeyStoreUtil(File file, String password) throws NoSuchAlgorithmException, CertificateException, IOException, JKSKeyStoreUtilException {
+		
+		try {
+			keyStore = KeyStore.getInstance("JKS");
+		} catch (KeyStoreException e) {
+			throw new JKSKeyStoreUtilException(e);
+		}
+		
+		FileInputStream fis = new FileInputStream(file);
+		keyStore.load(fis, password.toCharArray());
+		fis.close();
+	}
+	
+	/**
+	 * Saves changes
+	 * 
+	 * @throws JKSKeyStoreUtilException
+	 * @throws IOException
 	 */
-	static public JKSKeyStoreUtil open(File file, String password) throws NoSuchAlgorithmException, CertificateException, IOException, JKSKeyStoreUtilException {
-		return new JKSKeyStoreUtil(file, password, false);
+	public void save(File file, String password) throws JKSKeyStoreUtilException, IOException {
+		file.createNewFile();
+		
+		FileOutputStream fos = new FileOutputStream(file);
+		try {
+			logger.info("Saving to: " + file.getName());
+			keyStore.store(fos, password.toCharArray() );
+		} catch (Exception e) {
+			throw new JKSKeyStoreUtilException(e);
+		} finally {
+			fos.close();
+		}
+		
 	}
 	
 	/**
@@ -103,36 +102,84 @@ public class JKSKeyStoreUtil {
 		if (! Base64.isBase64(pem) ) {
 			throw new JKSKeyStoreUtilException("PEM File does not contain valid Base64 data");
 		}
+		
 		byte[] der = Base64.decodeBase64(pem);
 		try {
 			ByteArrayInputStream bais = new ByteArrayInputStream(der);
 			CertificateFactory cf = CertificateFactory.getInstance("X.509");
 			X509Certificate cert = (X509Certificate) cf.generateCertificate(bais); 
-			X500Principal x509principal = cert.getSubjectX500Principal();
-			String alias = x509principal.getName();
-			logger.info("Adding Cert with alias: " + alias);
-			keyStore.setCertificateEntry(alias, cert);
+			
+			String alias = getCertAlias(cert);
+			
+			if (alias == null) {
+				logger.warning(String.format("Can not suitable alias for cert %s", getDN(cert)));
+			} else if (containsAlias(alias)) {
+				logger.warning(String.format( "%s alias already exists (%s)", alias, getDN(cert) )); 
+			} else {
+				logger.fine("Adding Cert with alias: " + alias);
+				keyStore.setCertificateEntry(alias, cert);
+			}
 		} catch (Exception e) {
 			throw new JKSKeyStoreUtilException(e);
 		}
-		save();
 	}
 	
-	private void save() throws JKSKeyStoreUtilException, IOException {
-		if (!deferedSave) {
-			file.createNewFile();
+	
+	/**
+	 * @param alias						string to check for
+	 * @return							true if alias exists in Keystore
+	 * @throws KeyStoreException
+	 */
+	public boolean containsAlias(String alias) throws KeyStoreException {
+		return keyStore.containsAlias(alias);
+	}
+	
+	/**
+	 * Gets DN of a certificate
+	 * 
+	 * @param certificate
+	 * @return								full DN of a certificate
+	 */
+	private String getDN(X509Certificate certificate) {
+		X500Principal x509principal = certificate.getSubjectX500Principal();
+		String dn = x509principal.getName("RFC2253");
+		return dn;
+	}
+	
+	private String getCertAlias(X509Certificate certificate) {
+/*		String alias = "";
+		
+		String cn = null;
+		String o = null;
+		try {
+			cn = getCertAttribute(certificate, "cn");
+			o = getCertAttribute(certificate, "o");
+		} catch (NamingException e) {
 			
-			FileOutputStream fos = new FileOutputStream(file);
-			try {
-				logger.info("Saving to: " + file.getName());
-				keyStore.store(fos, password);
-			} catch (Exception e) {
-				throw new JKSKeyStoreUtilException(e);
-			} finally {
-				fos.close();
+		}
+		
+		if (cn != null ) alias += cn + ", ";
+		if (o != null ) alias += o;
+		
+		return alias;*/
+		
+		return getDN(certificate);
+	}
+	
+/*	private String getCertAttribute(X509Certificate certificate, String attributeName) throws NamingException {
+		String dn = getDN(certificate);
+		LdapName ln = new LdapName(dn);
+		
+		logger.finer(String.format("Getting %s from %s", attributeName, dn));
+		for (Rdn rdn : ln.getRdns()) {
+			String type = rdn.getType();
+			if (attributeName.toUpperCase().equals(type)) {
+				return (String) rdn.getValue();
 			}
 		}
-	}
+		return null;
+				
+	}*/
 	
 	/**
 	 * @param pemFile						imports a whole {@linkplain PEMFile} object
@@ -140,14 +187,65 @@ public class JKSKeyStoreUtil {
 	 * @throws IOException
 	 */
 	public void importPEMFile(PEMFile pemFile) throws JKSKeyStoreUtilException, IOException {
-		// disable saving until all imported
-		deferedSave = true;
 		ArrayList<String> pems = pemFile.getPEMBlocks();
 		
 		for (String pemString : pems) {
 			importPEM(pemString);
 		}
-		deferedSave = false;
-		save();
+	}
+	
+	/**
+	 * Imports certs and keys from a JKSKeyStoreUtil entries into this instance
+	 * 
+	 * @param importJKS						import JKS file				
+	 * @throws KeyStoreException 
+	 */
+	public void importJKSKeyStore(JKSKeyStoreUtil importJKSU) throws KeyStoreException {		
+		KeyStore importKS = importJKSU.getKeyStore();
+		Enumeration<String> aliases = importKS.aliases();
+		
+		while (aliases.hasMoreElements()) {
+			String oldalias = (String) aliases.nextElement();
+			X509Certificate cert = (X509Certificate) importKS.getCertificate(oldalias);
+			
+			String alias = getCertAlias(cert);
+			if (alias == null) {
+				logger.warning(String.format("No alias found for %s", cert.toString() ) );
+				continue;
+			}
+			
+			if (containsAlias(alias)) {
+				logger.warning(String.format("Alias %s already exists", alias));
+				continue;
+			}
+			
+			keyStore.setCertificateEntry(alias, cert);
+		}
+	}
+	
+	/**
+	 * @return								keyStore object 
+	 */
+	public KeyStore getKeyStore() {
+		return keyStore;
+	}
+	
+	/**
+	 * @return								Alias / Cert DN Hashmap contained in Keystore
+	 * @throws KeyStoreException
+	 */
+	public HashMap<String, String> list() throws KeyStoreException {
+		HashMap<String, String> result = new HashMap<String, String>();
+		Enumeration<String> aliasesE = keyStore.aliases();
+		
+
+		
+		while (aliasesE.hasMoreElements()) {
+			String alias = (String) aliasesE.nextElement();
+			X509Certificate cert = (X509Certificate) keyStore.getCertificate(alias);
+			String dn = getDN(cert);
+			result.put(alias, dn);
+		}
+		return result;
 	}
 }
--- src/uk/co/mccnet/keyutil/Main.java
+++ src/uk/co/mccnet/keyutil/Main.java
@@ -1,6 +1,12 @@
 package uk.co.mccnet.keyutil;
 
 import java.io.File;
+import java.security.KeyStoreException;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -8,7 +14,7 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.OptionGroup;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
@@ -18,39 +24,51 @@ import org.apache.commons.cli.PosixParser;
  *
  */
 public class Main {
-
-	private static Logger logger = Logger.getLogger(Main.class.getCanonicalName());
+	private static Logger logger = Logger.getLogger(Main.class.getName());
 	
 	/**
 	 * @param args
 	 * @throws Exception 
 	 */
 	
-	@SuppressWarnings("static-access")
 	public static void main(String[] args) throws Exception {
 
 		logger.setLevel(Level.INFO);
 		
 		Options options = new Options();
 		options.addOption("h", "help", false, "Show help");
-		options.addOption("F", "force", false, "Force actions");
-		
+		options.addOption("F", "force", false, "Force actions"); 
 		options.addOption("n", "new", false, "Create new Java Keystore");
-		options.addOption("q", "quiet", false, "Quiet");
-	
-		options.addOption(OptionBuilder.withLongOpt("password")
-				.withDescription("Keystore password")
-				.hasArg()
-				.isRequired()
-				.create("p"));
 		
-		Option ksFileOption = new Option("f", "keystore-file", true, "Keystore filename");
-		ksFileOption.setRequired(true);
-		options.addOption(ksFileOption);
+		Option passwordOption = new Option("p", "password", true, "Keystore (secret) password");
+		passwordOption.setRequired(true);
+		options.addOption(passwordOption);
 		
-		Option pemFileOption = new Option("i", "import-file", true, "PEM import filename");
-		pemFileOption.setRequired(true);
+		Option fileOption = new Option("f", "keystore-file", true, "Output keystore filename");
+		fileOption.setRequired(true);
+		fileOption.setArgName("keystore");
+		options.addOption(fileOption);
+		
+		OptionGroup ogLogging = new OptionGroup();
+		ogLogging.addOption(new Option("q", "quiet", false, "Quiet"));
+		ogLogging.addOption(new Option("d", "debug", false, "Debug"));		
+		options.addOptionGroup(ogLogging);
+		
+		OptionGroup ogMode = new OptionGroup();
+		ogMode.setRequired(true);
+		ogMode.addOption(new Option("l", "list", false, "List keys"));
+		ogMode.addOption(new Option("i", "import", false, "Import keys"));
+		options.addOptionGroup(ogMode);
+		
+		Option pemFileOption = new Option("e", "import-pem-file", true, "PEM import filenames");
+		pemFileOption.setArgs(Option.UNLIMITED_VALUES);
+		pemFileOption.setArgName("PEM_file [<PEM_files>..]");
 		options.addOption(pemFileOption);
+		
+		Option jksFileOption = new Option("j", "import-jks-file", true, "JKS import filename and password");
+		jksFileOption.setArgs(Option.UNLIMITED_VALUES);
+		jksFileOption.setArgName("JKS_file:password [<JKS_file:password>..]");
+		options.addOption(jksFileOption);
 		
 		CommandLineParser parser = new PosixParser();
 		CommandLine line = null;
@@ -61,8 +79,7 @@ public class Main {
 	    } catch( ParseException exp ) {
 	        // oops, something went wrong
 	       
-	    	System.out.println(exp.getMessage() + "\n");
-	       
+	    	System.out.println(exp.getMessage() + "\n");       
 	    	printHelp(options);
 	    }
 		
@@ -70,11 +87,19 @@ public class Main {
 	    	printHelp(options);
 	    }
 	    
-	    if (line.hasOption("q")) {
+	    // Ensure console logs > INFO
+	    //Handler consoleHandler = new ConsoleHandler();
+	    //consoleHandler.setLevel(Level.FINEST);
+	    //logger.addHandler(consoleHandler);
+	    
+	    if (line.hasOption("quiet")) {
 	    	logger.setLevel(Level.SEVERE);
-	    }
+	    } else if (line.hasOption("debug")) {
+	    	logger.setLevel(Level.FINEST);
+		}   
 	    	    
 	    File keyStore = new File(line.getOptionValue("keystore-file"));
+	    String password = line.getOptionValue("password");
 	    
 	    if ( line.hasOption("new") && keyStore.exists() && ! line.hasOption("force")) {
 	    	throw new Exception("New Keystore - File already exists. Use --force to overwrite");
@@ -82,14 +107,55 @@ public class Main {
 			throw new Exception(String.format("%s does not exist. Create it with --new.", keyStore.getPath()));
 		}
 	    
-		File pemFile = new File(line.getOptionValue("import-file"));
-		PEMFile pf = new PEMFile(pemFile);
-		JKSKeyStoreUtil jksKeyStoreUtil = JKSKeyStoreUtil.newKeyStore(new File(line.getOptionValue("keystore-file")), line.getOptionValue("password"));
-		jksKeyStoreUtil.importPEMFile(pf);
+		JKSKeyStoreUtil jksKeyStoreUtil;
+		
+		if (line.hasOption("new") ) {
+			jksKeyStoreUtil = new JKSKeyStoreUtil();
+		} else {
+			// load existing
+			jksKeyStoreUtil = new JKSKeyStoreUtil(keyStore, password);
+		}
+		
+		if (line.hasOption("list")) {
+			list(jksKeyStoreUtil);
+		} else if (line.hasOption("import")) {
+			if (line.hasOption("import-pem-file")) {
+				for (String pemFileName : line.getOptionValues("import-pem-file")) {
+					File pemFile = new File(pemFileName);
+					PEMFile pf = new PEMFile(pemFile); 
+					jksKeyStoreUtil.importPEMFile(pf);
+				}
+			}
+			
+			if (line.hasOption("import-jks-file")) {
+				for (String jksFileArg : line.getOptionValues("import-jks-file")) {
+					String filePassArray[] = jksFileArg.split(":");
+					if (filePassArray.length != 2) {
+						throw new Exception("Invalid format of JKS file import argument");
+					}
+					File jksFile = new File(filePassArray[0]);
+					JKSKeyStoreUtil sourceKS = new JKSKeyStoreUtil(jksFile, filePassArray[1]); 
+					jksKeyStoreUtil.importJKSKeyStore(sourceKS);
+					logger.info(String.format("%s imported", jksFile.getName()));
+				}
+			}
+			
+			jksKeyStoreUtil.save(keyStore, password);
+		}	
+	}
+	
+	private static void list(JKSKeyStoreUtil jksKeyStoreUtil) throws KeyStoreException {
+		HashMap<String, String> aliasesCertsHash = jksKeyStoreUtil.list();
+		
+		Set<Entry<String, String>> aliasSet = aliasesCertsHash.entrySet();
+		for (Entry<String, String> entry : aliasSet) {
+			System.out.println(String.format("%s -- DN: %s", entry.getKey(), entry.getValue()));
+		}
 	}
 	
 	private static void printHelp(Options options) {
 		HelpFormatter helpFormatter = new HelpFormatter();
+		helpFormatter.setWidth(120);
 		
 		helpFormatter.printHelp("keyutil", options, true);
     	System.exit(1);
